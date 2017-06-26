efa2e4324f163cda90ac9db54c3ef38a4a20bcf3
Timber <Macroch@yandex.ru>
2017-06-27 00:07:46 +0300

    routes and material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AppRoutingModule } from './app.routes'
+// import { routes } from './app.routes';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
+
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
     RegisterComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
+    // RouterModule.forRoot( routes, {
+    //   useHash: true
+    // })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor( public router: RouterModule){}
+}
